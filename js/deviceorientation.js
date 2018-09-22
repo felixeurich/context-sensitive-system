e5e99d3860d73bb5e8729834db5fa01b21008c04
Felix Eurich <felix.eurich@novatec-gmbh.de>
2018-09-22 13:53:50 +0200

    Initial version

---
+++ js/deviceorientation.js
@@ -0,0 +1,39 @@
+class deviceorientation {
+	constructor(dataset)
+	{
+		this.dataset=dataset
+		window.addEventListener('deviceorientation', (...args) => this.devicemotionListener(...args))
+	}
+
+	devicemotionListener(event) {
+		(this.dataset.orientation = this.dataset.orientation || []).push(event);
+	}
+	
+	features()
+	{
+	  var f={}
+	  if((this.dataset.orientation = this.dataset.orientation || []).length>0)
+	 {
+	  var o=this.dataset.orientation
+	  var alpha=o.map(x=>x.alpha)
+	  var beta=o.map(x=>x.beta)
+	  var gamma=o.map(x=>x.gamma)
+
+		  var d3=o.map(x=>Math.sqrt((x.alpha-math.mean(alpha))**2+(x.beta-math.mean(beta))**2+(x.gamma-math.mean(gamma))**2))
+		f["mean"]=math.mean(d3);
+	  f["max"]=math.max(d3);
+	  f["sd"]=math.std(d3);
+f["min"]=math.min(d3);
+	  
+
+	 }
+	  
+	  return f;
+	}
+	
+	flush()
+	{
+	  this.dataset.orientation = [] ;
+	}
+	
+}
---
+++ js/jactivity.js
@@ -0,0 +1,98 @@
+class jActivity {
+	constructor(base, sensorClasses, callback, label, interval) {
+		this.callback = callback
+		this.label = label
+		this.interval = interval
+		this.base = base
+		var dataset = {}
+		this.dataset = dataset; 
+
+		var sensors = []
+		this.sensors = sensors; 
+
+
+		sensorClasses.forEach(function (sensorClass)
+		{
+			var sensor= new sensorClass(dataset);
+			sensors.push(sensor)
+		}
+		)
+		
+
+		// get stylesheet for transformation 
+		// TODO: could store this globally to not trigger recompilation 
+		var pmml2js = new Promise((resolve, reject) => {
+			var onSuccess = function(xsl_file) {
+				resolve( function(model){	
+					let generated_code = transform(model,xsl_file)
+					return eval(generated_code.textContent);
+				}
+				)
+			}
+			$.ajax({
+				type: "GET",
+				url: (this.base + "js/pmml2js.xsl"),
+				success: onSuccess
+			})
+		})
+
+		// get PMML 
+		var pmml = new Promise((resolve,reject) => {
+
+			var onSuccess = function(data) {
+				pmml=$.parseXML(data)
+				resolve(pmml)
+			}
+			var onError = function(jqXHR,textStatus,errorThrown ) {
+				alert(textStatus)
+				resolve(null)
+			}
+
+			$.ajax({
+				type: "GET",
+				url: (this.base + "models/classifier.pmml"),
+				success: onSuccess,
+				error: onError,
+			})
+/*
+			$.ajax({
+				type: "POST",
+				url: (this.base + "R/getPMML/json"),
+				data: 'json_data={"sensor": ' + JSON.stringify(this.sensors) + ',"label": ' + JSON.stringify(this.label) + ',"classifier": "rpart"}',
+				success: onSuccess,
+				dataType: "json"
+			})
+			*/
+		}
+		)
+
+		// once we have both we generate the classifier and register the callback
+		Promise.all([pmml2js,pmml]).then( p => 
+			{ 
+				this.classifier = p[0](p[1]) //generate and store classifiier 
+				// then call by interval assuming someone fills the dataset
+				window.setInterval(
+					function(scope) {
+						 // calculate features
+						 // TODO: currently only average!!!
+						 let features = {}
+
+						 for (var sensor in scope.sensors) {
+						  var sf=sensors[sensor].features()
+						  for (var feature in sf)
+						  {
+						    features[feature]=sf[feature]
+						  }
+						 }
+
+						 scope.callback(scope.classifier.evaluate(features))
+
+						for (var sensor in scope.sensors) {
+							sensors[sensor].flush()
+						}
+					 }
+					 , interval,this)
+			}
+		)
+	}
+}
